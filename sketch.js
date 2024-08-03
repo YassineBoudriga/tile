847734fcf90e3240beb915f4c25b5329704333f9
Yassine Boudriga <40839641+YassineBoudriga@users.noreply.github.com>
2024-08-03 14:01:19 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -33,9 +33,9 @@ function preload() {
 function setup() {
 
 
-  cnv = createCanvas(600, 600);
-  let newCanvasX = (windowWidth- img.width)/2;
-  let newCanvasY = (windowHeight- img.height)/2;
+  cnv = createCanvas(800, 800);
+  let newCanvasX = (windowWidth- width)/2;
+  let newCanvasY = (windowHeight- height)/2;
   cnv.position(newCanvasX,newCanvasY)
 
   img.resize(0,height);
