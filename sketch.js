1105d77dc5f5c20d44d74526d0cb216b9933f4fb
Yassine Boudriga <40839641+YassineBoudriga@users.noreply.github.com>
2024-08-03 14:21:51 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -78,12 +78,13 @@ function draw() {
   //setPalette();
     cols = img.width / resolution;
   rows = img.height / resolution;
-    let d = pixelDensity();
+  var d = pixelDensity();
+
 
   for(let i = 0; i < cols; i++){
     for(let j = 0; j < rows; j++){
       push();
-      let current_pixel = 4*((i * d * resolution) + (j * resolution) * d* img.width);
+      let current_pixel = 4*((i *d* resolution) + (j * resolution) * d* img.width);
       let c = color(img.pixels[current_pixel],
                     img.pixels[current_pixel + 1],
                     img.pixels[current_pixel + 2],
