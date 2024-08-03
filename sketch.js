9efdd5f86307edc3d56528645a5bf8631e1e86b3
Yassine Boudriga <40839641+YassineBoudriga@users.noreply.github.com>
2024-08-03 14:35:58 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -117,6 +117,7 @@ function handleFile(file){
 // Once the img element is created, use it to 
 // convert the image element into a p5Image object. 
 function imgCreated(){
+  img=undefined;
   imgelem.hide();
   // Create a temporary p5.Graphics object to draw the image.
   let g = createGraphics(imgelem.elt.width, imgelem.elt.height);
